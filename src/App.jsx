54b217876da2076148560297e28f7926df0a2080
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-08-06 22:11:49 +0530

    many files have been updated!!

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Collections_carousel from './components/Collections_carousel';
 import Bestseller from "./components/Bestseller";
+import Offer_tile from "./components/Offer_tile";
 import Special_collection from "./components/Special_collection";
 import Footer from "./components/Footer";
+import Up_arrow from "./components/Up_arrow";
 
 function App() {
   return (
     <div className="App">
+      {/* <Up_arrow /> */}
       <Header />
       <Banner />
       <Collections_carousel />
       <Bestseller />
+      <Offer_tile />
       <Special_collection />
       <Footer />
     </div>
