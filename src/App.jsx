7694f0eaa3ac9ccc27e8362fb0f73ba602ab84dc
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-08-06 20:38:04 +0530

    App.jsx has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Collections_carousel from './components/Collections_carousel';
+import Bestseller from "./components/Bestseller";
+import Special_collection from "./components/Special_collection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Header />
       <Banner />
       <Collections_carousel />
+      <Bestseller />
+      <Special_collection />
+      <Footer />
     </div>
   );
 }
