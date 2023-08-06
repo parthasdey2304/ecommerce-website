5abdec6d6fa4a443f350d77e9d957e7ab72df59b
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-08-06 16:06:00 +0530

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Collections_carousel from './components/Collections_carousel';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Collections_carousel />
     </div>
   );
 }
--- src/components/Collections_carousel.jsx
+++ src/components/Collections_carousel.jsx
@@ -1,6 +1,12 @@
 function Collections_carousel() {
     return (
-        <div></div>
+        <div className="w-full h-[400px] md:h-[800px] p-16">
+            <div className="bg-white w-full h-full overflow-x-scroll flex space-x-10 p-10">
+                <div className="h-full w-[400px] md:w-1/3 bg-blue-600"></div>
+                <div className="h-full w-[400px] md:w-1/3 bg-pink-300"></div>
+                <div className="h-full w-[400px] md:w-1/3 bg-red-600"></div>
+            </div>
+        </div>
     );
 }
 
