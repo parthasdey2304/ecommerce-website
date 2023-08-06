6b4690f7a9a9f8d3030c73afe20a39ddc45fcdf7
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-08-06 03:18:31 -0400

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,31 +5,31 @@ function Header() {
                 <div className="flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center">
                     {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" /> */}
-                    <span className="self-center text-4xl md:text-7xl font-semibold whitespace-nowrap text-gray-800"><span className="text-red-600">Snea</span>karz</span>
+                    <span className="self-center text-4xl md:text-7xl font-semibold whitespace-nowrap text-gray-800 font-serif"><span className="text-red-600">Snea</span>karz</span>
                 </a>
                 <div className="flex items-center md:order-2">
                     <button type="button" className="flex mr-3 text-sm bg-white rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span className="sr-only">Open user menu</span>
-                    <img className="w-8 md:w-16 h-8 md:h-16 rounded-full bg-white border-[8px] border-black" src="/docs/images/people/profile-picture-3.jpg" alt="" />
+                    <div className="w-8 md:w-16 h-8 md:h-16 rounded-full bg-white border-[8px] border-black"></div>
                     </button>
                     {/* <!-- Dropdown menu -->  */}
                     <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                     <div className="px-4 py-3">
-                        <span className="block text-2xl text-gray-900 dark:text-white">Partha Sarathi Dey</span>
-                        <span className="block text-2xl  text-gray-500 truncate dark:text-gray-400">support@parthasarathidey.tech</span>
+                        <span className="block text-2xl text-gray-900 dark:text-white font-serif">Partha Sarathi Dey</span>
+                        <span className="block text-2xl  text-gray-500 truncate dark:text-gray-400 font-serif">support@parthasarathidey.tech</span>
                     </div>
                     <ul className="py-2 pr-10" aria-labelledby="user-menu-button">
                         <li>
-                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-serif">Dashboard</a>
                         </li>
                         <li>
-                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-serif">Settings</a>
                         </li>
                         <li>
-                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-serif">Earnings</a>
                         </li>
                         <li>
-                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                        <a href="#" className="block px-4 py-2 text-2xl text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white font-serif">Sign out</a>
                         </li>
                     </ul>
                     </div>
@@ -43,22 +43,22 @@ function Header() {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 md:mt-6" id="navbar-user">
                 <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                     <li>
-                    <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-red-600 md:p-0 md:text-3xl font-semibold" aria-current="page">Home</a>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-red-600 md:p-0 md:text-3xl font-semibold" aria-current="page font-serif">Home</a>
                     </li>
                     <li>
-                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold">About</a>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold font-serif">About</a>
                     </li>
                     <li>
-                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold">Products</a>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold font-serif">Products</a>
                     </li>
                     <li>
-                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold">Shop</a>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold font-serif">Shop</a>
                     </li>
                     <li>
-                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold">Blog</a>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold font-serif">Blog</a>
                     </li>
                     <li>
-                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold">Contact</a>
+                    <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-600 md:p-0 md:text-3xl font-semibold font-serif">Contact</a>
                     </li>
                 </ul>
                 </div>
